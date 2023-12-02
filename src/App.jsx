9a826ef9ca0c21d26258e35f7458ec3e1c4ebc06
shootle48 <lolicon2544@gmail.com>
2023-12-02 18:32:31 +0700

    Vice city V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Band from "./assets/Vice.png";
 
 export default function App() {
 
   return(
     <div className="min-h-screen backgroud">
         <Navbar activePage="home" />
+
+
+        <div className="ml-[46rem] mt-[15rem]">
+          <img src={Band} alt="Band"/>
+        </div>
+
 
     </div>
   )
