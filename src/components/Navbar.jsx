fa48f551d85879aa5149070543253e4ef3fa707f
shootle48 <lolicon2544@gmail.com>
2023-12-03 18:42:26 +0700

    it 'Might' Finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ export default function Navbar({activePage}) {
         <div className="p-5 border-b border-fuchsia-100/20 flex flex-col md:flex-row md:justify-between">
             <img src={logo} alt="logo" className="w-[6rem]" />
 
-            <div className=" flex flex-col md:flex-row mt-6 gap-4">
+            {/* <div className=" flex flex-col md:flex-row mt-6 gap-4">
                 <a href="/" className={`${activePage == 'home' ? "text-fuchsia-200" : "text-black"}`}>Home</a>
                 <a href="/contact" className={`${activePage == 'contact' ? "text-fuchsia-200" : "text-black"}`}>Contact</a>
                 {activePage}
-            </div>
+            </div> */}
 
         </div>
     )
