82185253aa65c3f0a6f3528c60379044718e2b66
shootle48 <theprit.1316s@gmail.com>
2024-07-01 15:38:07 +0700

    update

---
+++ index.js
@@ -0,0 +1,6 @@
+// function plus() {
+//     const a = Number(document.getElementById('input1').value)
+//     const b = Number(document.getElementById('input2').value)
+//     const result = a+b
+//     document.getElementById('output').value = result
+// }
