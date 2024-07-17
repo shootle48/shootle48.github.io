87dbbab6f922f43718eabba647758028b25ad2d4
shootle48 <theprit.1316s@gmail.com>
2024-07-17 17:29:34 +0700

    update Colorcustom.js

---
+++ pokedex/Colorcustom.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          normal: "#a9a878",
+          fire: "#f07f2f",
+          water: "#6890f0",
+          electric: "#f8d030",
+          grass: "#78c84f",
+          ice: "#98d8d8",
+          fighting: "#c02f29",
+          poison: "#a040a1",
+          ground: "#e0c069",
+          flying: "#9781d7",
+          psychic: "#f85888",
+          bug: "#a8b821",
+          rock: "#b7a038",
+          ghost: "#705798",
+          dragon: "#7038f9",
+          dark: "#6f5848",
+          steel: "#b8b8d0",
+          fairy: "#f1b6bc",
+        },
+      },
+    },
+  };
